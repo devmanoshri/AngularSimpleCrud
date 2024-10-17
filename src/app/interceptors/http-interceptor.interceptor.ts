f387d87e903fe952bcf0f38c6e06624d01c4a744
devmanoshri <manoshri.no@gmail.com>
2024-10-17 13:54:39 +0200

    include PDF export of user and post list

---
+++ src/app/interceptors/http-interceptor.interceptor.ts
@@ -0,0 +1,48 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { LoaderService } from '../components/shared/loader/loader.service';
+import { MessagesService } from '../components/shared/messages/messages.service';
+import { MessageType } from '../components/shared/messages/messages.utils';
+
+@Injectable()
+export class HttpInterceptorInterceptor implements HttpInterceptor {
+  constructor(
+    private injector: Injector /*,private loaderService: LoaderService*/
+  ) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const loaderService = this.injector.get(LoaderService);
+    const messagesService = this.injector.get(MessagesService);
+    const err = new Error('test');
+    loaderService.showLoader = true;
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          loaderService.showLoader = false;
+        }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        messagesService.showMessage({
+          message: error.message,
+          type: MessageType.Danger,
+          id: 0,
+        });
+        loaderService.showLoader = false;
+        return throwError(() => err);
+      })
+    );
+  }
+}
